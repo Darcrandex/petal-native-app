249f75b1b1ed0819c8a293ed6febc894bd98e08f
darcrand <darcrandex@gmail.com>
2023-11-20 15:00:16 +0800

    🐞 fix(client): 解决 web 报错问题

--- client/app.config.ts
+++ client/app.config.ts
@@ -3,45 +3,34 @@ import { ConfigContext, ExpoConfig } from 'expo/config'
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
 
+  // expo-router 需要
   scheme: 'petal-app',
 
   name: 'petal-app',
   slug: 'petal-app',
   version: '1.0.0',
+
   orientation: 'portrait',
   icon: './src/assets/icon.png',
   userInterfaceStyle: 'light',
-  splash: {
-    image: './src/assets/splash.png',
-    resizeMode: 'contain',
-    backgroundColor: '#ffffff',
-  },
+  splash: { image: './src/assets/splash.png', resizeMode: 'contain', backgroundColor: '#ffffff' },
   assetBundlePatterns: ['**/*'],
-  ios: {
-    supportsTablet: true,
-  },
-  android: {
-    adaptiveIcon: {
-      foregroundImage: './src/assets/adaptive-icon.png',
-      backgroundColor: '#ffffff',
-    },
-  },
+  ios: { supportsTablet: true },
+  android: { adaptiveIcon: { foregroundImage: './src/assets/adaptive-icon.png', backgroundColor: '#ffffff' } },
+
+  // 开发阶段
+  // pc端的配置
   web: {
     favicon: './src/assets/favicon.png',
+    // 使用 metro 打包
+    bundler: 'metro',
   },
 
-  // SDK 49 需要添加
-  // 获取 tsconfig 中的 alias 配置
+  //SDK49需要添加//获取tsconfig中的alias配置
   experiments: { tsconfigPaths: true },
-
   plugins: [
     'expo-router',
-    [
-      'expo-camera',
-      {
-        cameraPermission: 'Allow $(PRODUCT_NAME) to access your camera.',
-      },
-    ],
+    ['expo-camera', { cameraPermission: 'Allow $(PRODUCT_NAME) to access your camera.' }],
     [
       'expo-image-picker',
       {
--- client/babel.config.js
+++ client/babel.config.js
@@ -2,6 +2,12 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel', ['module-resolver', { alias: { '@': './src' } }]],
+    plugins: [
+      'expo-router/babel',
+
+      // 文档中虽然只要求在 tsconfig 中配置
+      // 但其实还需要在 babel.config.js 中配置
+      ['module-resolver', { alias: { '@': './src' } }],
+    ],
   }
 }
