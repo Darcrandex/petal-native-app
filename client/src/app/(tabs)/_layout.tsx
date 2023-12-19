8812e604e69a56711d1132dc4b3eb5f02fef11b6
darcrand <darcrandex@gmail.com>
2023-12-19 09:22:43 +0800

    ✨ feat(client): 添加 fontawesome

--- client/src/app/(tabs)/_layout.tsx
+++ client/src/app/(tabs)/_layout.tsx
@@ -4,7 +4,11 @@
  * @author darcrand
  */
 
-import { AddIcon, BellIcon, HStack, Icon, MoonIcon, Pressable, VStack } from '@gluestack-ui/themed'
+import { faCompass, faUser } from '@fortawesome/free-regular-svg-icons'
+import { faCompass as faCompassRegular, faPlusCircle, faUser as faUserRegular } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
+import { HStack, Pressable, VStack, useToken } from '@gluestack-ui/themed'
 import { Slot, useRouter } from 'expo-router'
 import { useCallback } from 'react'
 import { useWindowDimensions } from 'react-native'
@@ -30,28 +34,21 @@ export default function TabsLayout() {
     }
   }, [validAuth, onOpen, router])
 
+  const $rose500 = useToken('colors', 'rose500')
+
   return (
     <>
       <VStack pb={safeAreaInsets.bottom} h={height}>
         <Slot />
 
         <HStack justifyContent='space-around' alignItems='center'>
-          <NavButton label='关注' icon={BellIcon} to='/' />
+          <NavButton label='关注' icon={faCompass} activeIcon={faCompassRegular} to='/' />
 
-          <Pressable
-            display='flex'
-            alignItems='center'
-            justifyContent='center'
-            borderRadius='$full'
-            w='$8'
-            h='$8'
-            bg='$rose500'
-            onPress={beforeOpen}
-          >
-            <Icon as={AddIcon} size='md' color='$white' />
+          <Pressable onPress={beforeOpen}>
+            <FontAwesomeIcon icon={faPlusCircle} color={$rose500} size={32} />
           </Pressable>
 
-          <NavButton label='我的' icon={MoonIcon} to='/mine' />
+          <NavButton label='我的' icon={faUser} activeIcon={faUserRegular} to='/mine' />
         </HStack>
       </VStack>
     </>
--- client/src/components/NavButton/index.tsx
+++ client/src/components/NavButton/index.tsx
@@ -4,20 +4,28 @@
  * @author darcrand
  */
 
-import { Icon, Pressable, Text, VStack } from '@gluestack-ui/themed'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { Pressable, Text, VStack, useToken } from '@gluestack-ui/themed'
 import { usePathname, useRouter } from 'expo-router'
 
-export type NavButtonProps = { label: string; icon?: any; to: string; replace?: boolean }
+export type NavButtonProps = { label: string; icon?: any; activeIcon?: any; to: string; replace?: boolean }
 
 export default function NavButton(props: NavButtonProps) {
   const pathname = usePathname()
   const router = useRouter()
   const isActive = pathname === props.to
 
+  const $secondary950 = useToken('colors', 'secondary950')
+  const $secondary300 = useToken('colors', 'secondary300')
+
   return (
     <Pressable onPress={() => (props.replace ? router.replace(props.to) : router.push(props.to))}>
-      <VStack p='$2' alignItems='center'>
-        <Icon as={props.icon} size='md' color={isActive ? '$secondary950' : '$secondary300'} />
+      <VStack p='$2' alignItems='center' space='xs'>
+        <FontAwesomeIcon
+          size={24}
+          icon={isActive ? props.activeIcon : props.icon}
+          color={isActive ? $secondary950 : $secondary300}
+        />
 
         <Text fontSize='$xs' lineHeight='$xs' color={isActive ? '$secondary950' : '$secondary300'}>
           {props.label}
